8be5b9048a751332daee5799a316a96ef11515d2
bdiniscia <bdiniscia@gmail.com>
2020-02-14 05:56:54 -0300

    Funciona recuperar links de un md. Resolve #3

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const path = require ("path");
+const path = require ('path');
+const chalk = require('chalk');
+const marked = require('marked')
 
 let file = process.argv[2]; // Toma el archivo que se le da en la consola
 file = path.resolve(file);  // Convierte la ruta de relativa a absoluta
 file = path.normalize(file);  // La estandariza
 
+
 const functTest = require('./module');
 
-if (path.extname(file) === '.md') {
+if (path.extname(file) === '.md') {  // Chequea si un archivo es .md antes de pasarlo
   const myPromise = functTest(file)
   .then((fileData) => {
     console.log(fileData);
@@ -15,7 +18,7 @@ if (path.extname(file) === '.md') {
     console.error(error)
   })
 } else {
-  console.log('Por favor, introduce un archivo .md')
+  console.log(chalk.bgRed('Por favor, introduce un archivo .md'))
 }
 
 
--- module.js
+++ module.js
@@ -1,17 +1,26 @@
-const fs = require ("fs");
-// const Marked = require ("Marked");
-
+const fs = require("fs");
+const marked = require("marked");
 
 const getFile = (file) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, 'utf8', (err, data) => {
-            if (err) {
-                return reject(err)
-            } else {
-                resolve(data)
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        return reject(err);
+      } else {
+        let links = [];
+        const renderer = new marked.Renderer();
+        renderer.link = function(href, title, text) {
+          links.push({
+            href: href,
+            text: text,
+            file: file,
+          });
+        };
+        marked(data, { renderer: renderer });
+        resolve(links);
+      }
+    });
+  });
+};
 
 module.exports = getFile;
