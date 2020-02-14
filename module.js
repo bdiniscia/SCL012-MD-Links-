d57b99a4b4e4cb57cd55117e853ad7a9bb22026d
bdiniscia <bdiniscia@gmail.com>
2020-02-14 10:04:18 -0300

    Muestra los estados de los links en consola y valida si son http. Resolve #5, resolve #6, resolve #7

--- module.js
+++ module.js
@@ -1,9 +1,10 @@
-const fs = require("fs");
-const marked = require("marked");
+const fs = require('fs');
+const marked = require('marked');
+const request = require('request');
 
 const getFile = (file) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, "utf8", (err, data) => {
+    fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
         return reject(err);
       } else {
@@ -16,11 +17,23 @@ const getFile = (file) => {
             file: file,
           });
         };
-        marked(data, { renderer: renderer });
+        marked(data, { renderer: renderer });  // Aquí imprime y crea los elementos dentro del Array
+        links = linksHttp(links);
         resolve(links);
       }
     });
   });
+};
+
+const linksHttp = (links) => {
+	let validateLink = [];
+	links.map((element) => {
+		let prefix = element.href.substring(0, 4);
+		if (prefix === 'http') {
+			validateLink.push(element);
+		}
+	})
+	return validateLink;
 };
 
 module.exports = getFile;
