ed7608dcf237b65f15d45db0348fc40cb37689ee
bdiniscia <bdiniscia@gmail.com>
2020-02-14 11:21:21 -0300

    Aplicar Prettier y comentar el código

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
-const path = require ('path');
+const path = require('path');
 const chalk = require('chalk');
 const fetch = require('node-fetch');
 
 let file = process.argv[2]; // Toma el archivo que se le da en la consola
-file = path.resolve(file);  // Convierte la ruta de relativa a absoluta
-file = path.normalize(file);  // La estandariza
+file = path.resolve(file); // Convierte la ruta de relativa a absoluta
+file = path.normalize(file); // La estandariza
 
+const functTest = require('./module'); // Mi función de module.js
 
-const functTest = require('./module');
-
-if (path.extname(file) === '.md') {  // Chequea si un archivo es .md antes de pasarlo
+// Función que llama a la promesa
+if (path.extname(file) === '.md') {
+  // Chequea si un archivo es .md antes de pasarlo
   const myPromise = functTest(file)
-  .then((fileData) => {
-    codeLinkStatus(fileData);
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+    .then(fileData => {
+      codeLinkStatus(fileData);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 } else {
-  console.log(chalk.bgRed('Por favor, introduce un archivo .md'))
+  console.log(chalk.bgRed('Por favor, introduce un archivo .md'));
 }
 
-const codeLinkStatus = (links) => {
-	links.map((element) => {
-		fetch(element.href)
-			.then(response => {
-				if (response.status == 200 || response.status == 201 || response.status == 202) {
-					console.log(chalk.green('[✔]'), chalk.cyan(element.href), chalk.bgGreen(` ${response.status} ${response.statusText} `), chalk.yellow(element.text));
-				} else {
-					console.log(chalk.red('[X]'), chalk.cyan(element.href), chalk.bgRed(` ${response.status} ${response.statusText} `), chalk.white(element.text));
-				}
-			}).catch((error) => console.log(chalk.gray('[-]'), chalk.cyan(element.href), chalk.bgRed(` ${error.type} ${error.code} `), chalk.white(element.text)));
-	})
-}
-
-
-  //myPromise.then()
+//  Función que chequea el status de cada link
+const codeLinkStatus = links => {
+  links.map(element => {
+    fetch(element.href)
+      .then(response => {
+        if (response.ok) {
+          console.log(
+            chalk.green('[✔]'),
+            chalk.cyan(element.href),
+            chalk.bgGreen(` ${response.status} ${response.statusText} `),
+            chalk.yellow(element.text)
+          );
+        } else {
+          console.log(
+            chalk.red('[X]'),
+            chalk.cyan(element.href),
+            chalk.bgRed(` ${response.status} ${response.statusText} `),
+            chalk.white(element.text)
+          );
+        }
+      })
+      .catch(error =>
+        console.log(
+          chalk.gray('[-]'),
+          chalk.cyan(element.href),
+          chalk.bgRed(` ${error.type} ${error.code} `),
+          chalk.white(element.text)
+        )
+      );
+  });
+};
--- module.js
+++ module.js
@@ -1,23 +1,24 @@
 const fs = require('fs');
 const marked = require('marked');
-const request = require('request');
 
-const getFile = (file) => {
+// Mi función que me lee y manipula el archivo
+const getFile = file => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
         return reject(err);
       } else {
         let links = [];
-        const renderer = new marked.Renderer();
+        const renderer = new marked.Renderer(); // Get reference
         renderer.link = function(href, title, text) {
+          // Override function
           links.push({
             href: href,
             text: text,
-            file: file,
+            file: file
           });
         };
-        marked(data, { renderer: renderer });  // Aquí imprime y crea los elementos dentro del Array
+        marked(data, { renderer: renderer }); // Aquí imprime y crea los elementos dentro del Array
         links = linksHttp(links);
         resolve(links);
       }
@@ -25,15 +26,16 @@ const getFile = (file) => {
   });
 };
 
-const linksHttp = (links) => {
-	let validateLink = [];
-	links.map((element) => {
-		let prefix = element.href.substring(0, 4);
-		if (prefix === 'http') {
-			validateLink.push(element);
-		}
-	})
-	return validateLink;
+// Filtra y retorna un nuevo array con los links que contienen 'http'
+const linksHttp = links => {
+  let validateLink = [];
+  links.map(element => {
+    let prefix = element.href.substring(0, 4);
+    if (prefix === 'http') {
+      validateLink.push(element);
+    }
+  });
+  return validateLink;
 };
 
-module.exports = getFile;
+module.exports = getFile; // Exporta la función principal
