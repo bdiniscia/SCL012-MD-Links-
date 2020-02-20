8368ed7d61cb03a18fe7322083541300a421ca5b
bdiniscia <bdiniscia@gmail.com>
2020-02-19 22:16:05 -0300

    Separando lógica. Trabajando WIP de #16 y close #18

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ const mdlinks = (file, options) => {
       }
 
       if (options.validate === true && options.stats === false) {
-        return resolve(codeLinkStatus(links, undefined));
+        codeStatusLinks(links)
+          .then(links => {
+            resolve(links);
+          })
+          .catch(err => console.log(err));
       }
 
       if (options.validate === false && options.stats === true) {
@@ -52,53 +56,42 @@ const linksHttp = links => {
   });
 };
 
-//  Función que chequea el status de cada link
-// const codeLinkStatus = (links) => {
-
-//       for(let i = 0; i<links.length; i++) {
-//         console.log(links[i].href)
-//         fetch(links[i].href)
-//           .then(response => {
-//           console.log(response.statusText);
-//           links[i].status = response.status;
-//           links[i].statusText = response.statusText;
-//         }).catch(error => {
-//           links[i].status = error.type;
-//           links[i].statusText = error.code;
-//         });
-//       }
-// };
 
 //  Función que chequea el status de cada link
-const codeLinkStatus = (links, newLinks) => {
-  
-    if (newLinks === undefined) {
-      newLinks = [];
-    }
 
-    if (links.length < 1) { // La condición que para la función
-      return newLinks;
-    }
+const codeStatusLinks = (links) => {
+  return new Promise((resolve, reject) => {
+    const getCodeStatusLinks = (links, newLinks) => {
+      if (newLinks === undefined) {
+        newLinks = [];
+      }
+    
+      if (links.length < 1) {
+        // La condición que para la función
+        return resolve(newLinks);
+      }
+    
+      let actualLink = links[0]; // El link a evaluar siempre
+      links.shift(); // Quita el primer elemento del Array luego de asignarlo a actualLink
+      fetch(actualLink.href) // Promesa para evaluar cada link y agregar en el contador los rotos
+        .then(response => {
+          actualLink.status = response.status;
+          actualLink.statusText = response.statusText;
+          newLinks.push(actualLink);
+          getCodeStatusLinks(links, newLinks); // Se vuelve a llamar a si misma si pasa al then
+        })
+        .catch(error => {
+          actualLink.status = error.type;
+          actualLink.statusText = error.code;
+          newLinks.push(actualLink);
+          getCodeStatusLinks(links, newLinks); // Se vuelve a llamar a si misma si cae en error
+        });
+    };
 
-    let actualLink = links[0]; // El link a evaluar siempre
-    console.log(">> here: 1");
-    links.shift(); // Quita el primer elemento del Array luego de asignarlo a actualLink
-    fetch(actualLink.href) // Promesa para evaluar cada link y agregar en el contador los rotos
-      .then(response => {
-        console.log(">> here: 2");
-        actualLink.status = response.status;
-        actualLink.statusText = response.statusText;
-        newLinks.push(actualLink);
-        codeLinkStatus(links, newLinks); // Se vuelve a llamar a si misma si pasa al then
-      })
-      .catch(error => {
-        console.log(">> here: 3");
-        actualLink.status = error.type;
-        actualLink.statusText = error.code;
-        newLinks.push(actualLink);
-        codeLinkStatus(links, newLinks); // Se vuelve a llamar a si misma si cae en error
-      });
+    getCodeStatusLinks(links);
+  });
 };
+
 
 // Función de "-s" y "-v -s"
 const linksStats = (links, isBothOptions) => {
